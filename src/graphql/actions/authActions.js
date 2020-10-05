e775fa4931860ffe5ba57e8ef14973df729938a8
carlossanchezg <scarlosdaniel@live.com>
2020-10-05 12:04:15 -0600

    implementar try catch en authActions

--- src/graphql/actions/authActions.js
+++ src/graphql/actions/authActions.js
@@ -3,32 +3,40 @@ import { createToken, comparePasswords } from '../../utils';
 
 export default {
   register: async (body) => {
-    const userExist = await userActions.getUserByEmail(body.email);
-    if (userExist) throw new Error('Usuario existente inicia sesión');
+    try {
+      const userExist = await userActions.getUserByEmail(body.email);
+      if (userExist) throw new Error('Usuario existente inicia sesión');
 
-    const newUser = await userActions.createUser(body);
+      const newUser = await userActions.createUser(body);
 
-    const token = createToken(newUser);
-    if (!token) throw new Error('token error');
+      const token = createToken(newUser);
+      if (!token) throw new Error('token error');
 
-    return {
-      user: newUser,
-      token,
-    };
+      return {
+        user: newUser,
+        token,
+      };
+    } catch (error) {
+      return error;
+    }
   },
   login: async (body) => {
-    const userExist = await userActions.getUserByEmail(body.email);
-    if (!userExist) throw new Error('Usuario no existenge registrate primero');
+    try {
+      const userExist = await userActions.getUserByEmail(body.email);
+      if (!userExist) throw new Error('Usuario no existenge registrate primero');
 
-    const comparedPasswords = await comparePasswords(body.password, userExist.password);
-    if (!comparedPasswords) throw new Error('contraseña incorrecta');
+      const comparedPasswords = await comparePasswords(body.password, userExist.password);
+      if (!comparedPasswords) throw new Error('contraseña incorrecta');
 
-    const token = createToken(userExist);
-    if (!token) throw new Error('token error');
+      const token = createToken(userExist);
+      if (!token) throw new Error('token error');
 
-    return {
-      user: userExist,
-      token,
-    };
+      return {
+        user: userExist,
+        token,
+      };
+    } catch (error) {
+      return error;
+    }
   },
 };
